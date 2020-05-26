514f2c002cd5b5f66a23501b30226f71fcc647c7
Jbradley-C <jbradley.chapman@gmail.com>
2020-05-26 17:01:08 -0500

    first 24% of drill complete

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
 // Handler for .ready() called.
 $( document ).ready(function() {
     
+    $("#btnSubmit").click(function(e){
+        alert("kjlkkjlkajljlklj");
 
+    });
 
+    $("form").submit(function() {
+        e.prevemDefault();
+    //    alert($("input[name="name"]").val());
 
+        $("div").append($(`<h2>${$("input[name=name"]").val()}</h2>`));
+        $("h2").mouseover(function(){
+            $(this).css({
+                "background-color": "yellow"
+                "border-radius": (Math.random() *50) + "px"
+        });
 
+    });
 
+    $("input[name="name" ] ").on("keyup", function() {
+        if ($("input[name= "name"]").val() ==="") {
+            $("#btnSubmit").attr("disabled", true);
+        }else {
+            $("#btnSubmit").attr("disabled", false);
+        }
+        
 
-
-
-
-
-
-
-
+    });
     
+    $("body").append($("<div></div>"));
+
+
+
+
+
+
+
+
   });
